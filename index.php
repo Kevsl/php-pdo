335bb952d55663c8d85f4f103fc1e5b913a769b7
Kevsl <ktr2@pm.me>
2024-03-13 10:12:25 +0100

    added login feature- back end side- without front

--- index.php
+++ index.php
@@ -23,7 +23,50 @@ $categories = $categoryManager->getAllCategories();
     <title>Document</title>
 </head>
 
-<body>
+<body class="relative">
+
+    <div class="modal absolute top-0 bg-white w-screen h-screen z-20">
+    <div class="flex  flex-col justify-center px-6 py-12 lg:px-8  w-1/2 mx-auto ">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="space-y-6">
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3 emailInput">
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          <div class="text-sm">
+            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+          </div>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 indent-3 passwordInput">
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" onclick="handleLogin()"  class=" flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">Sign in</button>
+      </div>
+    </div>
+
+    <p class="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+    </p>
+  </div>
+</div>
+</div>
+
+    <main class="main hidden">
     <h1 class="font-bold text-3xl text-center my-6">
         TD ECOMMERCE
     </h1>
@@ -34,7 +77,7 @@ $categories = $categoryManager->getAllCategories();
 
         foreach ($products as $product) {
             echo "
-        <div class='w-1/6 h-64 pb-4 mx-8 bg-gray-300  border border-gray-300 border-2 rounded-xl my-8 relative'>
+        <div class='w-1/6 h-64 pb-4 mx-8 bg-gray-300  border border-gray-300 border-2 rounded-xl my-8 relative z-0'>
         <img src='" . $product->getImage_products() . "' class='w-full h-3/4 object-cover rounded-xl'/>
         <p class=' pl-4 mt-2 h-12 truncate'>" . $product->getName_products() . " 
         <div class='flex items-center justify-between pr-4 '>
@@ -139,7 +182,7 @@ $categories = $categoryManager->getAllCategories();
 
 
     </div>
-
+    </main>
 </body>
 
 
@@ -148,5 +191,5 @@ $categories = $categoryManager->getAllCategories();
 
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="https://kit.fontawesome.com/ff684f1294.js" crossorigin="anonymous"></script>
-
+<script src="./script.js"></script>
 </html>
